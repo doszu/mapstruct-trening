353e7ec5efdcae1d9ef9bfb8a6fb078d2fa6b33d
doszu <doszu.dev@gmail.com>
2020-06-24 15:08:18 +0200

    after training

--- src/main/java/com/example/mapstruct/mapper/Example5Mapper.java
+++ src/main/java/com/example/mapstruct/mapper/Example5Mapper.java
@@ -6,16 +6,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-//from version 1.4.0.Beta1
 @Mapper
 public interface Example5Mapper {
 
     Example5Mapper INSTANCE = Mappers.getMapper(Example5Mapper.class);
 
+    //from version 1.4.0.Beta1
     @Mapping(source = "props", target = ".")
     Example5Target toTarget(Example5Source source);
 
 
+    // 1.3.1
     @Mapping(source = ".", target = "props")
     Example5Source toSource(Example5Target target);
 
